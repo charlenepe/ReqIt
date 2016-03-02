1672f80b68609c106586c3e6974ca88e417df2d1
Charlene Pe <charlenepe@alumni.ateneo.edu>
2016-03-02 12:44:48 -0800

    Initial Commit

---
+++ ReqItApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReqItApp
+//
+//  Created by Charlene Pe on 3/2/16.
+//  Copyright © 2016 Charlene Pe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
