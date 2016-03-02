df5a6f8d9d2bc619c11077ea0f007a55236b0ea5
Charlene Pe <charlenepe@alumni.ateneo.edu>
2016-03-02 15:10:12 -0800

    First commit

---
+++ Pods/Target Support Files/Pods-ReqItApp/Pods-ReqItApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReqItApp : NSObject
+@end
+@implementation PodsDummy_Pods_ReqItApp
+@end
---
+++ ReqItApp/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  devslopes-showcase
+//
+//  Created by Mark Price on 8/21/15.
+//  Copyright © 2015 devslopes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR:CGFloat = 157.0 / 255.0
