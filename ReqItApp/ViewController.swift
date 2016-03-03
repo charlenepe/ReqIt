eecf61314d7260b6bc2e0b721b52edf28bfb9b93
Charlene Pe <charlenepe@alumni.ateneo.edu>
2016-03-02 22:35:09 -0800

    Revised

--- ReqItApp/ViewController.swift
+++ ReqItApp/ViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
 
-class ViewController: UIViewController {
+
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.emailField.delegate = self
+        self.passwordField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,6 +29,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if NSUserDefaults.standardUserDefaults().valueForKey(KEY_UID) != nil {
+            self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+        }
+    }
+    
     
     @IBAction func fbBtnPressed(sender: UIButton){
         let facebookLogin = FBSDKLoginManager()
@@ -42,19 +59,65 @@ class ViewController: UIViewController {
                     } else {
                         print("Logged in! \(authData)")
                         NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: KEY_UID)
-                        self.performSegueWithIdentifier("loggedIn", sender: nil)
+                        self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
                     }
                     
                 })
                 
-        
-            
+            }
         }
-
-        
+}
+    
+    @IBAction func attemptLogin(sender: UIButton!){
+        if let email = emailField.text where email != "", let pwd = passwordField.text where pwd != "" {
+            
+            DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: { error, authData in
+                
+                if error != nil {
+                    print(error.code)
+                    
+                    if error.code == STATUS_ACCOUNT_NONEXIST {
+                        DataService.ds.REF_BASE.createUser(email, password: pwd, withValueCompletionBlock: { error, result  in
+                    
+                            if error != nil {
+                                self.showErrorAlert("Could not create account", msg: "Problem creating account")
+                            } else {
+                                NSUserDefaults.standardUserDefaults().setValue(result[KEY_UID], forKey: KEY_UID)
+                                
+                                DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: nil)
+                                self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+                            }
+                            
+                        })
+                    } else {
+                        self.showErrorAlert("Could not log in", msg: "Please check username or password")
+                    }
+                    
+                } else {
+                    self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+                }
+                
+            })
+            
+            
+        } else {
+            showErrorAlert("Email and password", msg: "You must enter an email and a password")
+        }
     }
+    
+    func showErrorAlert(title: String, msg: String){
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+       textField.resignFirstResponder()
+        return true
+    }
+    
+}
 
-}
-}
 
 
